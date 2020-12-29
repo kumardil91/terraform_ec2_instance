0c3a9105bc782e6758da01e33808fb5fd3680d3c
Dileep Kumar <dileep.kumar@nectechnologies.in>
2020-12-29 23:31:10 +0530

    add the variables for az

--- main.tf
+++ main.tf
@@ -17,12 +17,38 @@ module "vpc" {
    tags = var.vpc_tags
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.main.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
 
 resource "aws_instance" "app" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
   ami               = "ami-0a0ad6b70e61be944"
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  key_name               = "user1"
+  vpc_security_group_ids = aws_security_group.allow_tls.id
   subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
@@ -35,9 +61,19 @@ resource "aws_instance" "app" {
               #!/bin/bash
               sudo yum install -y httpd
               sudo yum install -y php 
+              sudo yum install -y git
               sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
               sudo service httpd start
               EOF
 }
 
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
 
+  bucket_name = "testtrerte"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
